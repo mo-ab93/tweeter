a153d5dced688d135a078c61f16b580b6ce0701b
Mohammed Abbas <mohammed.abbas1193@gmail.com>
2022-08-17 21:39:55 -0400

    fetch data with Ajax, Form Validation, Refetch tweets on Submission,Use an escape function, Displaying Validation Errors With jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,44 +1,18 @@
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-
-  ]
-
   const renderTweets = function (tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-    let result = '';
+    $("#tweets-container").empty();
     for (let tweet of tweets) {
-      result += createTweetElement(tweet);
-
+      const result = createTweetElement(tweet);
+      $("#tweets-container").prepend(result);
     }
 
-    return $("#tweets-container").append(result);
   }
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
   const createTweetElement = function (tweet) {
     let $tweet = `
@@ -51,12 +25,12 @@ $(document).ready(function () {
   <h4>${tweet["user"].handle}</h4>
 </header>
 <div class="tweet-content">
-  <p>${tweet["content"].text}</p>
+  <p>${escape(tweet["content"].text)}</p>
 </div>
 
 <footer>
 
-  <p>${tweet["created_at"]}</p>
+  <p>${timeago.format(tweet["created_at"])}</p>
 
   <div>
     <i class="fa-solid fa-flag"></i>
@@ -72,21 +46,34 @@ $(document).ready(function () {
   }
 
 
-  renderTweets(data);
 
+  const loadTweets = function() {
+    $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+    .then(function (data) {
+      renderTweets(data);
+    });
+  }
+  loadTweets();
+
+  $(".bar.error").hide();
   
   $("#tweet-input").submit(function( event ) {
     event.preventDefault();
-    
-    $.ajax({
-      type: 'POST',
-      url: "/tweets/",
-      data: $(this).serialize(),   
-      success: function(){
-         renderTweets(data);
-      }
-      
-   });
+    const tweetLength = $("#tweet-text").val().length;
+    const remaining = 140 - tweetLength;
+    const emptyInput = $("#tweet-text").val().trim()
+    if ( emptyInput === "" || remaining < 0) {
+      $(".bar.error").slideDown( "slow" );
+      return;
+    }
+      $.ajax({
+        type: 'POST',
+        url: "/tweets/",
+        data: $(this).serialize(),
+        success: function(){
+          loadTweets();
+        }
+     });
    
   });
 
